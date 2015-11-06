d69cfd6ba11686358c11c13f1404609c1a380971
Juan Pablo Genovese <juanpgenovese@gmail.com>
2015-11-06 11:29:31 -0300

    Add views and index

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require "sinatra/reloader"
+
+configure do
+  set :views,         File.dirname(__FILE__) + '/views'
+  set :public_folder, File.dirname(__FILE__) + '/public'
+end
 
 get '/' do
-  host = ENV['VCAP_APP_HOST']
-  port = ENV['VCAP_APP_PORT']
-  <<-EOS
-  <h3>Sinatra Test app for CF Vagrant Installer</h3>
-      Hello from #{host}:#{port}! <br/>
-  EOS
+  @host = ENV['CF_INSTANCE_IP'] || "192.168.0.0"
+  @port = ENV['CF_INSTANCE_PORT'] || "9292"
+  @index = ENV['CF_INSTANCE_INDEX'] || "0"
+
+  erb :index
 end
